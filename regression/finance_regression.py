c8aeb275fc36fbb877aa45ecb9970c7e2d4c06e0
utkarshyadavin <utkarshyadavin@gmail.com>
2017-10-05 21:47:06 +0530

    fitted the data using sklearn

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -15,6 +15,7 @@
 
 import sys
 import pickle
+from sklearn import linear_model
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,6 +39,10 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+reg = linear_model.LinearRegression()
+reg.fit (feature_train , target_train)
+print "Coefficient: " , reg.coef_
+print "intercept: " , reg.intercept_
 
 
 
